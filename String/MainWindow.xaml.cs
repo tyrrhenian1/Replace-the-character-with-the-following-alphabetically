ab23529f376137a20c2049d533e6a9772c449c9a
Артем Лазарчик <lazarchik_2000@mail.ru>
2020-11-30 23:03:19 +0200

    all

--- String/MainWindow.xaml.cs
+++ String/MainWindow.xaml.cs
@@ -15,9 +15,7 @@ using System.Windows.Shapes;
 
 namespace String
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
         public MainWindow()
